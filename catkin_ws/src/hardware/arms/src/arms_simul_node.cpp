24baf1c504359a0db230b9fd223173d5780752c5
Diego <mago22darck@hotmail.com>
2017-12-07 10:31:46 -0600

    head_simul_node  arms_simul_node  and  legs_simul_node improved and ready to use

--- catkin_ws/src/hardware/arms/src/arms_simul_node.cpp
+++ catkin_ws/src/hardware/arms/src/arms_simul_node.cpp
@@ -1,6 +1,9 @@
 #include "ros/ros.h" 
 #include "std_msgs/Float32.h"
 #include "dynamixel_sdk/dynamixel_sdk.h"
+#include "ros/ros.h" 
+#include "std_msgs/Float32MultiArray.h"
+#include "dynamixel_sdk/dynamixel_sdk.h"
 #include <sensor_msgs/JointState.h>
 #include <tf/transform_broadcaster.h>
 #include <iostream>
@@ -12,57 +15,85 @@
 #include <fcntl.h>
 #include <termios.h>
 
-#define DEVICE_NAME      "/dev/ttyUSB0"
-#define BAUDRATE         57600
-#define PROTOCOL_VERSION 1.0
+#define DEVICE_NAME                      "/dev/ttyUSB0"
+#define BAUDRATE                         57600
+#define PROTOCOL_VERSION                 1.0
 
-#define SERVO_MX_RANGE_IN_BITS 4096
-#define SERVO_MX_RANGE_IN_RADS 2*M_PI
-#define SERVO_MX_BITS_PER_RAD  SERVO_MX_RANGE_IN_BITS/SERVO_MX_RANGE_IN_RADS
+#define SERVO_MX_RANGE_IN_BITS           4096
+#define SERVO_MX_RANGE_IN_RADS           2*M_PI
+#define SERVO_MX_BITS_PER_RAD            SERVO_MX_RANGE_IN_BITS/SERVO_MX_RANGE_IN_RADS
 
-#define ARM_LEFT_YAW_ZERO     2048
-#define ARM_LEFT_YAW_CW       1
+#define ARM_LEFT_SHOULDER_PITCH_ZERO     2048
+#define ARM_LEFT_SHOULDER_PITCH_CW          1
 
-uint16_t goal_position;
-bool new_goal_position = false;
+#define ARM_LEFT_SHOULDER_ROLL_ZERO      2048
+#define ARM_LEFT_SHOULDER_ROLL_CW           1
 
-float conv2 (float b)
+#define ARM_LEFT_ELBOW_PITCH_ZERO        2048
+#define ARM_LEFT_ELBOW_PITCH_CW             1
+
+#define ARM_RIGHT_SHOULDER_PITCH_ZERO    2048
+#define ARM_RIGHT_SHOULDER_PITCH_CW         1
+
+#define ARM_RIGHT_SHOULDER_ROLL_ZERO     2048
+#define ARM_RIGHT_SHOULDER_ROLL_CW          1
+
+#define ARM_RIGHT_ELBOW_PITCH_ZERO       2048
+#define ARM_RIGHT_ELBOW_PITCH_CW            1
+
+#define SERVO_MX_STEP_SIMUL                30
+
+uint16_t goal_position [6];
+
+int position_zero_bits[6] = {
+    ARM_LEFT_SHOULDER_PITCH_ZERO,
+    ARM_LEFT_SHOULDER_ROLL_ZERO,
+    ARM_LEFT_ELBOW_PITCH_ZERO,
+    ARM_RIGHT_SHOULDER_PITCH_ZERO,
+    ARM_RIGHT_SHOULDER_ROLL_ZERO,
+    ARM_RIGHT_ELBOW_PITCH_ZERO 
+};
+
+int clockwise_direction[6] = {
+    ARM_LEFT_SHOULDER_PITCH_CW,
+    ARM_LEFT_SHOULDER_ROLL_CW,
+    ARM_LEFT_ELBOW_PITCH_CW,
+    ARM_RIGHT_SHOULDER_PITCH_CW,
+    ARM_RIGHT_SHOULDER_ROLL_CW,
+    ARM_RIGHT_ELBOW_PITCH_CW
+};
+
+void callback_goal_pose(const std_msgs::Float32MultiArray::ConstPtr& msg)
 {
-  float radbit = 0.0; //Variable para conversión rad a bit
-  radbit = 651.739492*b+2047.5; //Conversión de rad a bit
-  radbit = round(radbit); //Redondea el valor de bit
-  uint16_t entero = 0; //Variable de la posición deseada
-  entero = int(radbit); //Convierte al flotante en entero
-  return entero;
+    for(int i=0; i < 6; i++)
+    goal_position[i] = uint16_t(msg->data[i] * 4096/(2*M_PI) * clockwise_direction[i] + position_zero_bits[i]);
 }
 
-void callback_goal_pose(const std_msgs::Float32::ConstPtr& msg)
+void callback_goal_pose_left(const std_msgs::Float32MultiArray::ConstPtr& msg)
 {
-    std::cout << "ArmLeft.->New goal position: " << msg->data << std::endl;
-    goal_position = conv2(msg->data);
-    new_goal_position = true;
+    for(int i=0; i < 6; i++)
+    goal_position[i] = uint16_t(msg->data[i] * 4096/(2*M_PI) * clockwise_direction[i] + position_zero_bits[i]);
+}
+void callback_goal_pose_right(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+    for(int i=3; i < 6; i++)
+    goal_position[i] = uint16_t(msg->data[i - 3] * 4096/(2*M_PI) * clockwise_direction[i] + position_zero_bits[i]);
 }
 
 int main(int argc, char** argv)
  {     
-    //Inicializamos el publicador de simulación
-    ros::init(argc, argv, "state_publisher");
-    ros::NodeHandle n1;
-    ros::Publisher joint_pub = n1.advertise<sensor_msgs::JointState>("joint_states", 1);
-    tf::TransformBroadcaster broadcaster;
+    //Initilize joint_states_publisher
+    std::cout << "INITIALIZING ARMS NODE..." << std::endl;
+    ros::init(argc, argv, "state_publisher"); 
+    ros::init(argc, argv, "arms");
+    ros::NodeHandle n;
+    ros::Rate loop(30);
+    ros::Subscriber subArmsGoalPose     = n.subscribe("arms_goal_pose", 1, callback_goal_pose);
+    ros::Subscriber subArmLeftGoalPose  = n.subscribe("arm_left_goal_pose", 1, callback_goal_pose_left);
+    ros::Subscriber subArmRightGoalPose = n.subscribe("arm_right_goal_pose", 1, callback_goal_pose_right);
+    ros::Publisher  joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
 
-    // message declarations
-    geometry_msgs::TransformStamped odom_trans;
     sensor_msgs::JointState joint_states_arms;
-   
-    odom_trans.header.frame_id = "odom";
-    odom_trans.child_frame_id = "trunk";
- 
-    //odom_trans.header.stamp = ros::Time::now();
-    odom_trans.transform.translation.x = 0.0;//cos(angle)*2;
-    odom_trans.transform.translation.y = 0.0;//sin(angle)*2;
-    odom_trans.transform.translation.z = 0.0;//.7;
-    odom_trans.transform.rotation = tf::createQuaternionMsgFromYaw(0);
   
     joint_states_arms.name.resize(6);
     joint_states_arms.position.resize(6);
@@ -75,59 +106,35 @@ int main(int argc, char** argv)
     joint_states_arms.name[4] ="right_joint_shoulder_roll";
     joint_states_arms.name[5] ="right_joint_elbow_pitch";
 
-    ros::init(argc, argv, "arms");
-    ros::NodeHandle n;
-    ros::Rate loop(1000);
-    ros::Subscriber subGoalPose = n.subscribe("goal_pose_arms", 1, callback_goal_pose);
-
-    
-	std::cout << "LegLeft.->Serial port successfully openned" << std::endl;
-    
-	std::cout << "LegLeft.->Cannot open serial port" << std::endl;
-	
-	std::cout << "LegLeft.->Baudrate successfully set " << std::endl;
-
-
-    int problem_counter = 0;
-    int total_counter = 0;
-
-    uint16_t dxl_current_pos_sim = 0;
+    uint16_t dxl_current_pos_sim [6];
+    for(int i=0; i < 6; i++)
+    {
+        goal_position[i] = position_zero_bits[i];
+        dxl_current_pos_sim[i] = position_zero_bits[i];
+    }
 
     while(ros::ok())
     {
- 
-      std::cout << "Current position: " << (int)dxl_current_pos_sim << std::endl;
+        for(int i=0; i < 6; i++)
+        {
+            int error = goal_position[i] - dxl_current_pos_sim[i];
+            if(error >  SERVO_MX_STEP_SIMUL) error =  SERVO_MX_STEP_SIMUL;
+            if(error < -SERVO_MX_STEP_SIMUL) error = -SERVO_MX_STEP_SIMUL;
+            dxl_current_pos_sim[i] += error;
+        }
+  
+        joint_states_arms.header.stamp = ros::Time::now();
 
-      joint_states_arms.header.stamp = ros::Time::now();
-      joint_states_arms.position[0] = ((int)(dxl_current_pos_sim) - ARM_LEFT_YAW_ZERO)*ARM_LEFT_YAW_CW * (2*M_PI / 4096.0);
-      joint_states_arms.position[1] = 0;
-      joint_states_arms.position[2] = 0;
-      joint_states_arms.position[3] = 0;
-      joint_states_arms.position[4] = 0;
-      joint_states_arms.position[5] = 0;
-
-      // (moving in a circle with radius=2)
-      odom_trans.header.stamp = ros::Time::now();
-
-      // //send the joint state and transform
-      joint_pub.publish(joint_states_arms);
-      broadcaster.sendTransform(odom_trans);
-
-      if((dxl_current_pos_sim - goal_position) > 0)
-      {
-      	dxl_current_pos_sim--;
-      }
-
-      if((dxl_current_pos_sim - goal_position) < 0)
-      {
-      	dxl_current_pos_sim++;
-      }
-       
-      if(new_goal_position)
-	{
-	  new_goal_position = false;
-	}
-      ros::spinOnce();
-      loop.sleep();
+        for(int i=0; i < 6; i++)
+        {
+            joint_states_arms.position[i] = ((int)(dxl_current_pos_sim[i]) - position_zero_bits[i]) * clockwise_direction[i] * 
+            (2*M_PI/4096.0);
+        }
+  
+        //Send the joint state and transform
+        joint_pub.publish(joint_states_arms);
+        
+        ros::spinOnce();
+        loop.sleep();
     }
-}
+  } 
--- catkin_ws/src/hardware/legs/src/legs_simul_node.cpp
+++ catkin_ws/src/hardware/legs/src/legs_simul_node.cpp
@@ -12,67 +12,54 @@
 #include <fcntl.h>
 #include <termios.h>
 
-#define DEVICE_NAME      "/dev/ttyUSB0"
-#define BAUDRATE         57600
-#define PROTOCOL_VERSION 1.0
+#define DEVICE_NAME      			"/dev/ttyUSB0"
+#define BAUDRATE         			57600
+#define PROTOCOL_VERSION 			1.0
 
-#define SERVO_MX_RANGE_IN_BITS 4096
-#define SERVO_MX_RANGE_IN_RADS 2*M_PI
-#define SERVO_MX_BITS_PER_RAD  SERVO_MX_RANGE_IN_BITS/SERVO_MX_RANGE_IN_RADS
+#define SERVO_MX_RANGE_IN_BITS 		4096
+#define SERVO_MX_RANGE_IN_RADS 		2*M_PI
+#define SERVO_MX_BITS_PER_RAD  		SERVO_MX_RANGE_IN_BITS/SERVO_MX_RANGE_IN_RADS
 
-#define LEG_LEFT_YAW_ZERO     2048
-#define LEG_LEFT_YAW_CW       1
+#define LEG_LEFT_YAW_ZERO			2048
+#define LEG_LEFT_YAW_CW  			   1
 
-#define LEG_LEFT_PITCH_ZERO     2048
-#define LEG_LEFT_PITCH_CW       1
+#define LEG_LEFT_PITCH_ZERO     	2048
+#define LEG_LEFT_PITCH_CW              1
 
-#define LEG_LEFT_ROLL_ZERO     2048
-#define LEG_LEFT_ROLL_CW       1
+#define LEG_LEFT_ROLL_ZERO     		2048
+#define LEG_LEFT_ROLL_CW               1
 
-#define LEG_LEFT_KNEE_PITCH_ZERO     2048
-#define LEG_LEFT_KNEE_PITCH_CW       1
+#define LEG_LEFT_KNEE_PITCH_ZERO    2048
+#define LEG_LEFT_KNEE_PITCH_CW         1
 
-#define LEG_LEFT_ANKLE_PITCH_ZERO     2048
-#define LEG_LEFT_ANKLE_PITCH_CW       1
+#define LEG_LEFT_ANKLE_PITCH_ZERO   2048
+#define LEG_LEFT_ANKLE_PITCH_CW        1
 
-#define LEG_LEFT_ANKLE_ROLL_ZERO     2048
-#define LEG_LEFT_ANKLE_ROLL_CW       1
+#define LEG_LEFT_ANKLE_ROLL_ZERO    2048
+#define LEG_LEFT_ANKLE_ROLL_CW         1
 
 
-#define LEG_RIGHT_YAW_ZERO     2048
-#define LEG_RIGHT_YAW_CW       1
+#define LEG_RIGHT_YAW_ZERO			2048
+#define LEG_RIGHT_YAW_CW       		   1
 
-#define LEG_RIGHT_PITCH_ZERO     2048
-#define LEG_RIGHT_PITCH_CW       1
+#define LEG_RIGHT_PITCH_ZERO     	2048
+#define LEG_RIGHT_PITCH_CW       	   1
 
-#define LEG_RIGHT_ROLL_ZERO     2048
-#define LEG_RIGHT_ROLL_CW       1
+#define LEG_RIGHT_ROLL_ZERO     	2048
+#define LEG_RIGHT_ROLL_CW       	   1
 
-#define LEG_RIGHT_KNEE_PITCH_ZERO     2048
-#define LEG_RIGHT_KNEE_PITCH_CW       1
+#define LEG_RIGHT_KNEE_PITCH_ZERO   2048
+#define LEG_RIGHT_KNEE_PITCH_CW        1
 
-#define LEG_RIGHT_ANKLE_PITCH_ZERO     2048
+#define LEG_RIGHT_ANKLE_PITCH_ZERO  2048
 #define LEG_RIGHT_ANKLE_PITCH_CW       1
 
-#define LEG_RIGHT_ANKLE_ROLL_ZERO     2048
-#define LEG_RIGHT_ANKLE_ROLL_CW       1
+#define LEG_RIGHT_ANKLE_ROLL_ZERO   2048
+#define LEG_RIGHT_ANKLE_ROLL_CW        1
 
 #define SERVO_MX_STEP_SIMUL 30
 
-uint16_t goal_position [12] = {
-    LEG_LEFT_YAW_ZERO, 
-    LEG_LEFT_PITCH_ZERO, 
-    LEG_LEFT_ROLL_ZERO, 
-    LEG_LEFT_KNEE_PITCH_ZERO, 
-    LEG_LEFT_ANKLE_PITCH_ZERO,
-    LEG_LEFT_ANKLE_ROLL_ZERO, 
-    LEG_RIGHT_YAW_ZERO, 
-    LEG_RIGHT_PITCH_ZERO, 
-    LEG_RIGHT_ROLL_ZERO, 
-    LEG_RIGHT_KNEE_PITCH_ZERO, 
-    LEG_RIGHT_ANKLE_PITCH_ZERO, 
-    LEG_RIGHT_ANKLE_ROLL_ZERO 
-};
+uint16_t goal_position [12];
 
 int position_zero_bits[12] = {
     LEG_LEFT_YAW_ZERO, 
@@ -103,8 +90,6 @@ int clockwise_direction[12] = {
     LEG_RIGHT_ANKLE_PITCH_CW, 
     LEG_RIGHT_ANKLE_ROLL_CW 
 };
-
-
 
 void callback_goal_pose(const std_msgs::Float32MultiArray::ConstPtr& msg)
 {
@@ -156,50 +141,35 @@ int main(int argc, char** argv)
     joint_state_legs.name[11] ="right_joint_ankle_roll";
 
     uint16_t dxl_current_pos_sim [12];
+
     for(int i=0; i < 12; i++)
     {
-	goal_position[i] = position_zero_bits[i];
-	dxl_current_pos_sim[i] = position_zero_bits[i];
+		goal_position[i] = position_zero_bits[i];
+		dxl_current_pos_sim[i] = position_zero_bits[i];
     }
     
     while(ros::ok())
     {
-	for(int i=0; i < 12; i++)
-	{
-	    int error = goal_position[i] - dxl_current_pos_sim[i];
-	    if(error >  SERVO_MX_STEP_SIMUL) error =  SERVO_MX_STEP_SIMUL;
-	    if(error < -SERVO_MX_STEP_SIMUL) error = -SERVO_MX_STEP_SIMUL;
-	    dxl_current_pos_sim[i] += error;
-	}
+		for(int i=0; i < 12; i++)
+		{
+	    	int error = goal_position[i] - dxl_current_pos_sim[i];
+	    	if(error >  SERVO_MX_STEP_SIMUL) error =  SERVO_MX_STEP_SIMUL;
+	    	if(error < -SERVO_MX_STEP_SIMUL) error = -SERVO_MX_STEP_SIMUL;
+	    	dxl_current_pos_sim[i] += error;
+		}
 	
-	joint_state_legs.header.stamp = ros::Time::now();
-	for(int i=0; i < 12; i++)
-	{
-	    joint_state_legs.position[i] = ((int)(dxl_current_pos_sim[i]) - position_zero_bits[i]) * clockwise_direction[i] *
-		(2*M_PI/4096.0);
-	}
-	
-	//Send the joint state and transform
-	joint_pub.publish(joint_state_legs);
-	
-	//This logic works but uses lots of lines of code
-	/*
-        // 0      if((dxl_current_pos_sim_0 - goal_position_0) > SERVO_MX_STEP_SIMUL)
-	dxl_current_pos_sim_0 -= SERVO_MX_STEP_SIMUL;
+		joint_state_legs.header.stamp = ros::Time::now();
 
-	if((dxl_current_pos_sim_0 - goal_position_0) < -SERVO_MX_STEP_SIMUL)
-	dxl_current_pos_sim_0 += SERVO_MX_STEP_SIMUL;
+		for(int i=0; i < 12; i++)
+		{
+	    	joint_state_legs.position[i] = ((int)(dxl_current_pos_sim[i]) - position_zero_bits[i]) * clockwise_direction[i] *
+			(2*M_PI/4096.0);
+		}
 	
-	if(abs(dxl_current_pos_sim_0 - goal_position_0) <= SERVO_MX_STEP_SIMUL && (dxl_current_pos_sim_0 - goal_position_0) > 0)
-	dxl_current_pos_sim_0 -= abs(dxl_current_pos_sim_0 - goal_position_0);
-	
-	if(abs(dxl_current_pos_sim_0 - goal_position_0) <= SERVO_MX_STEP_SIMUL && (dxl_current_pos_sim_0 - goal_position_0) < 0)
-	dxl_current_pos_sim_0 += abs(dxl_current_pos_sim_0 - goal_position_0);
-	*/
-      
-	ros::spinOnce();
-	loop.sleep();
+		//Send the joint state and transform
+		joint_pub.publish(joint_state_legs);
+    	  
+		ros::spinOnce();
+		loop.sleep();
     }
-}
-
-
+ }
