2d9f0b2f587b7d8b4daad1f3f395230955e12863
knight <levacarrillo@gmail.com>
2021-02-27 20:49:18 -0600

    Added GAZEBO_MODEL_PATH as enviroment variable into launch

--- SetupDraft.sh
+++ SetupDraft.sh
@@ -1,12 +1,10 @@
-sudo apt-get install ros-melodic-gazebo-ros-control
-sudo apt-get install ros-melodic-ros-controllers
-sudo apt-get install ros-melodic-ros-control
-sudo apt-get install ros-melodic-controller-manager
-sudo apt-get install ros-melodic-effort-controllers
-sudo apt-get install ros-melodic-joint-state-controller
-sudo apt-get install ros-melodic-moveit-kinematics
-sudo apt-get install ros-melodic-dynamixel-sdk
-sudo apt-get install ros-melodic-moveit-core
-sudo apt-get install ros-melodic-moveit-ros-planning-interface
-
-#echo "export GAZEBO_MODEL_PATH=~/Humanoids/catkin_ws/src/hardware/nimbro_op_gazebo/models/" >> /home/$USER/.bashrc
+sudo apt-get install ros-melodic-gazebo-ros-control -y
+sudo apt-get install ros-melodic-ros-controllers -y
+sudo apt-get install ros-melodic-ros-control -y
+sudo apt-get install ros-melodic-controller-manager -y
+sudo apt-get install ros-melodic-effort-controllers -y
+sudo apt-get install ros-melodic-joint-state-controller -y
+sudo apt-get install ros-melodic-moveit-kinematics -y
+sudo apt-get install ros-melodic-dynamixel-sdk -y
+sudo apt-get install ros-melodic-moveit-core -y
+sudo apt-get install ros-melodic-moveit-ros-planning-interface -y
