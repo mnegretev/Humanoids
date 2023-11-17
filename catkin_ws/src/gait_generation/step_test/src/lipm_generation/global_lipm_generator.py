35b297fd819f0aa43a8dfdfee5e87d8fbaf59480
Miguel Garcia <garcia.miguel.onate@gmail.com>
2023-11-16 20:56:51 -0600

    fix axis inverted in lipm_generator script

--- catkin_ws/src/gait_generation/step_test/src/lipm_generation/global_lipm_generator.py
+++ catkin_ws/src/gait_generation/step_test/src/lipm_generation/global_lipm_generator.py
@@ -6,13 +6,13 @@ import math
 
 G = 9.81 # [m/s^2]
 
-X_BODY_TO_FEET  = 0.12 # [m]
+Y_BODY_TO_FEET  = 0.12 # [m]
 Z_ROBOT_WALK    = 0.68 # m
 Z_ROBOT_STATIC= 0.75 # m
 
 # stepHeight = 0.2
 STEP_LENGTH = 0.2 # [m]
-ROBOT_VEL_Y = 0.1 # [m]
+ROBOT_VEL_X = 0.1 # [m]
 
 # Tiempo de muestreo
 SAMPLE_TIME = 0.0001 # [s]
@@ -26,11 +26,8 @@ class Step:
     def __init__(self, mode=None):
         self.com_rel_position = []
         
-        if mode in Step.stepmode:
-            self.mode = mode
-        else:
-            self.mode = None
-
+        assert mode in Step.stepmode
+        self.mode = mode
         self.timevec = []
         self.footleft = []
         self.footright = []
@@ -41,13 +38,12 @@ class Step:
 
 
 def main():
-
     steps = []
 
-    x_0 = X_BODY_TO_FEET
-    [dx0, y_0, dy0, single_support_time] = findInitialConditions(STEP_LENGTH,
-                                                                 ROBOT_VEL_Y,
-                                                                 x_0,
+    y_0 = Y_BODY_TO_FEET
+    [dy0, x_0, dx0, single_support_time] = findInitialConditions(STEP_LENGTH,
+                                                                 ROBOT_VEL_X,
+                                                                 y_0,
                                                                  Z_ROBOT_WALK,
                                                                  G)
 
@@ -63,9 +59,10 @@ def main():
     #This is a harcoded value that can be modified later
 
     starting_body_points = np.array([
-                     [0,0.67*X_BODY_TO_FEET],
+                     [0,-0.67*Y_BODY_TO_FEET],
                      [0,0],
-                     [Z_ROBOT_STATIC, Z_ROBOT_WALK]])
+                     [Z_ROBOT_STATIC, Z_ROBOT_WALK]]
+                    )
 
     timepoints = [0,1]
 
@@ -74,28 +71,28 @@ def main():
     m_x = (starting_body_points[0][1] - starting_body_points[0][0])/(timepoints[1]-timepoints[0])
     m_z = (starting_body_points[2][1] - starting_body_points[2][0])/(timepoints[1]-timepoints[0])
 
-    q_x = interpolate.interp1d(time_vector, time_vector*m_x*SAMPLE_TIME, fill_value="extrapolate")
-    q_y = 0
+    q_x = 0
+    q_y = interpolate.interp1d(time_vector, time_vector*m_x*SAMPLE_TIME, fill_value="extrapolate")
     q_z = interpolate.interp1d(time_vector, 
                                starting_body_points[2][0] + time_vector*m_z*SAMPLE_TIME,
                                fill_value="extrapolate")
 
     for i in np.arange(0,len(time_vector)):
-        aux = np.array([[q_x(i), q_y, q_z(i)]])
+        aux = np.array([[q_x, q_y(i), q_z(i)]])
         body_position = np.concatenate((body_position,aux), axis=0)
     body_position = np.delete(body_position, 0, axis=0)
 
-    rel_position = body_position - [-X_BODY_TO_FEET, 0, 0]
+    rel_position = body_position - [0, -Y_BODY_TO_FEET, 0]
 
-    foot_right = np.array([[-X_BODY_TO_FEET,0,0]])
-    foot_left = np.array([[X_BODY_TO_FEET,0,0]])
+    foot_right = np.array([[0,-Y_BODY_TO_FEET,0]])
+    foot_left = np.array([[0, Y_BODY_TO_FEET,0]])
     
     for i in np.arange(0, len(time_vector)-1):
-        foot_right = np.concatenate((foot_right, np.array([[-x_0, 0, 0]])), axis=0)
-        foot_left = np.concatenate((foot_left, np.array([[x_0, 0 , 0]])), axis=0)
-
-    # print(f"Relative position COM matrix shape: {rel_position.shape}")
-    # print(rel_position)
+        foot_right = np.concatenate((foot_right, np.array([[0, -y_0, 0]])), axis=0)
+        foot_left = np.concatenate((foot_left, np.array([[0, y_0 , 0]])), axis=0)
+    
+    print(f"Absolute COM shape is: {rel_position.shape}")
+    print(rel_position)
     print(f"Right foot matrix shape: {foot_right.shape}")
     print(foot_right)
     print(f"Left foot matrix shape: {foot_left.shape}")
@@ -107,6 +104,11 @@ def main():
     start_pose_step.footright = foot_right
 
     steps.append(start_pose_step)
+    
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.plot(body_position[:,0],body_position[:,1],body_position[:,2], "o")    
+    plt.show()
+    exit()
 
     #################################################################
     ## PART 2: MAKE A HALF STEP
@@ -189,30 +191,30 @@ def main():
     
     plt.show()
 
-def findInitialConditions(STEP_LENGTH, ROBOT_VEL_Y, x_0, zModel, G):
+def findInitialConditions(STEP_LENGTH, ROBOT_VEL_X, y_0, zModel, G):
     #Desired midstance and state
     s = math.sqrt(zModel/G)
-    y_mid = 0
+    x_mid = 0
     # using relationship between final body state and initial body state,
     # we can find time it will take to re
     #Corresponding orbital energy is
-    E = -G/(2*zModel) * (y_mid**2) + 0.5*(ROBOT_VEL_Y**2)
-    y_0 = -STEP_LENGTH/2
+    E = -G/(2*zModel) * (x_mid**2) + 0.5*(ROBOT_VEL_X**2)
+    x_0 = -STEP_LENGTH/2
     #Finding dy0 from midstance energy level
     # using relationship between final body state and initial body state,
     # we can find time it will take to re
-    dy0 = math.sqrt(2*(E + G/(2*zModel) * (y_0**2)))
+    dx0 = math.sqrt(2*(E + G/(2*zModel) * (x_0**2)))
     # using relationship between final body state and initial body state,
     # we can find time it will take to reach midstance given final velocity
     # (dy = ROBOT_VEL_Y) and final position (which is y = 0 at midstance)
-    tsinglesupport = 2*math.asinh( STEP_LENGTH/2/(s*ROBOT_VEL_Y) ) * s
-    print("singlesupportTIme is ", tsinglesupport)
+    singlesupport_t = 2*math.asinh( STEP_LENGTH/2/(s*ROBOT_VEL_X) ) * s
+    print("singlesupportTIme is ", singlesupport_t)
 
-    tf = tsinglesupport/2
+    tf = singlesupport_t/2
 
-    dx0 = -x_0/s * math.sinh(tf/s) / math.cosh(tf/s)
+    dy0 = -y_0/s * math.sinh(tf/s) / math.cosh(tf/s)
 
-    return [dx0, y_0, dy0, tsinglesupport]
+    return [dy0, x_0, dx0, singlesupport_t]
 
 def changeLeg(finalState, body_position):
     xf  =   finalState[0]
