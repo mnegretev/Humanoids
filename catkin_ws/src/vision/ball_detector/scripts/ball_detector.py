37a2560ced6aaca7037ce5f4048423d7397b305c
Andrés López Esquivel <lopezesquivelandres1@gmail.com>
2023-12-04 19:45:23 -0600

    Update node ball_detector to calculate the angles roll, pitch and yaw that indicate the orientation of the new frame whose x-axis goes through the center of the ball.

--- catkin_ws/src/vision/ball_detector/scripts/ball_detector.py
+++ catkin_ws/src/vision/ball_detector/scripts/ball_detector.py
@@ -38,6 +38,42 @@ def get_undistorted_px_by_service(distorted_point):
     # to undistort a pixel
     return distorted_point
 
+def get_roll_pitch_yaw(centroid_x,
+                       centroid_y,
+                       img_x_center,
+                       img_y_center,
+                       width_resolution,
+                       height_resolution,
+                       hfov_rad,
+                       vfov_rad):
+  # https://themetalmuncher.github.io/fov-calc/
+  # hfov = 150 degrees, vfov = 130 degrees
+  k_hfov = hfov_rad / width_resolution
+  k_vfov = vfov_rad / height_resolution
+  return [0, # roll
+          (centroid_y - img_y_center) * k_vfov, # pitch
+          (img_x_center - centroid_x) * k_hfov] # yaw
+
+def get_quaternion_from_euler(roll, pitch, yaw):
+  # https://computergraphics.stackexchange.com/questions/8195/how-to-convert-euler-angles-to-quaternions-and-get-the-same-euler-angles-back-fr
+  """
+  Convert an Euler angle to a quaternion.
+   
+  Input
+    :param roll: The roll (rotation around x-axis) angle in radians.
+    :param pitch: The pitch (rotation around y-axis) angle in radians.
+    :param yaw: The yaw (rotation around z-axis) angle in radians.
+ 
+  Output
+    :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
+  """
+  qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+  qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+  qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+  qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+ 
+  return [qx, qy, qz, qw]
+
 def get_undistorted_image(distorted_image, camera_matrix, distortion_coef):
     return cv2.undistort(distorted_image, camera_matrix, distortion_coef, None)
 
@@ -52,6 +88,18 @@ def callback_calculate_new_camera_frame(msg):
     distorted_centroid = get_centroid_px_from_binary_img(binary_img = binary_image)
     # Get the ball undistorted centroid in pixels
     x_undistorted, y_undistorted = get_undistorted_px_by_service(distorted_point = distorted_centroid)
+    # Get the angles roll, pitch and yaw that indicate the orientation of the new frame
+    roll, pitch, yaw = get_roll_pitch_yaw(centroid_x = x_undistorted,
+                                          centroid_y = y_undistorted,
+                                          img_x_center = 640,
+                                          img_y_center = 360,
+                                          width_resolution = 1550,
+                                          height_resolution = 1290,
+                                          hfov_rad = 2.6180,
+                                          vfov_rad = 2.2689)
+    # Get the anglesr roll, pitch and yaw as a quaternion q
+    q = get_quaternion_from_euler(roll = roll, pitch = pitch, yaw = yaw)
+
     # == BEGIN: DEBUGGING SECTION ==
     undistorted_image = get_undistorted_image(distorted_image = distorted_image,
                                               camera_matrix   = CAMERA_MATRIX,
