10a77370a3f252b72b34ae467714c97c7f053773
Ruth Moreno <ruthmced@gmail.com>
2024-04-12 02:05:33 +0000

    Check Pan and error in head_goal_position_node

--- catkin_ws/src/vision/goal_detection/scripts/head_goal_ball_position_node.py
+++ catkin_ws/src/vision/goal_detection/scripts/head_goal_ball_position_node.py
@@ -11,39 +11,62 @@ from geometry_msgs.msg import *
 from cv_bridge import CvBridge   
 from matplotlib import pyplot as plt
 from geometry_msgs.msg import Point32
-
+from std_msgs.msg import Float32MultiArray
 def head_k_generator(centroid_msg):
     center_x=centroid_msg.x
     center_y=centroid_msg.y
+    head_cmd = Float32MultiArray()
     Cx=320
     Cy=240
     ex=center_x-Cx
     ey=center_y-Cy
-    Kpan=1/320
-    Ktilt=0.5/240
-    if ex>0: #Ball is at right
-        pan=(Kpan*ex)*(-1)
-        if ey<0: #Ball is up right
-            tilt=(Ktilt*ey)*(-1)
+    Kpan=0.5/320
+    Ktilt=0.25/240
+    while center_x!=0 and center_y!=0:
+        if ex > 0 and ey < 0: #Ball is up right
+            pan=(Kpan*ex)
+            tilt=(Ktilt*ey)
             print ("Ball is up right : ",[pan,tilt])
-        if ey>0: #Ball is down right
+            print ("Center is: ",[center_x,center_y])
+            goal=[pan,tilt]
+            head_cmd.data=goal
+            pub_head_goal.publish(head_cmd) 
+        elif ex > 0 and ey > 0: #Ball is down right
+            pan=(Kpan*ex)
             tilt=(Ktilt*ey)
             print ("Ball is down right : ",[pan,tilt])
-    if ex<0: #Ball is at left
-        pan=(Kpan*ex)
-        if ey<0: #Ball is up left
-            tilt=(Ktilt*ey)*(-1)
+            print ("Center is: ",[center_x,center_y])
+            goal=[pan,tilt]
+            head_cmd.data=goal
+            pub_head_goal.publish(head_cmd) 
+        elif ex < 0 and ey < 0: #Ball up left
+            pan=(Kpan*ex)
+            tilt=(Ktilt*ey)
             print ("Ball is up left : ",[pan,tilt])
-        if ey>0: #Ball is down left
+            print ("Center is: ",[center_x,center_y])
+            goal=[pan,tilt]
+            head_cmd.data=goal
+            pub_head_goal.publish(head_cmd)
+        elif ex < 0 and ey > 0: #Ball is down left
+            pan=(Kpan*ex)
             tilt=(Ktilt*ey)
             print ("Ball is down left : ",[pan,tilt])
-    if 315 <= center_x <= 325 and 235 <= center_y <= 245:
-        print ("Ball is centering : ",[pan,tilt])
+            print ("Center is: ",[center_x,center_y])
+            goal=[pan,tilt]
+            head_cmd.data=goal
+            pub_head_goal.publish(head_cmd) 
+        elif 315 <= center_x <= 325 and 235 <= center_y <= 245:
+            print ("Ball is centering : ",[center_x,center_y]) 
 
-
+        else:
+            print("Any ball detected")
+        break        
+        #return pan,tilt
 def main ():
+    global pub_head_goal,rate
     rospy.init_node("head_goal_ball_position_node")  
     rospy.Subscriber("/centroid_publisher", Point32, head_k_generator)
+    pub_head_goal = rospy.Publisher("/hardware/head_goal_pose", Float32MultiArray, queue_size=1)   
     rospy.spin()
 
 if __name__=="__main__":
