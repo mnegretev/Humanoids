3bad2d80b779aab163329d92a623c1010869afc8
Miguel Garcia <garcia.miguel.onate@gmail.com>
2025-01-23 01:08:30 +0000

    done fixing branches

--- catkin_ws/src/vision/goal_detection/scripts/goal_detection_node.py
+++ catkin_ws/src/vision/goal_detection/scripts/goal_detection_node.py
@@ -2,8 +2,10 @@
 import rospy
 import numpy
 import cv2
-import ros_numpy
+#import ros_numpy
 import math
+import random
+import argparse
 from std_msgs.msg import *
 from sensor_msgs.msg import *
 from geometry_msgs.msg import *
@@ -11,19 +13,127 @@ from cv_bridge import CvBridge
 from matplotlib import pyplot as plt
 
 def callback_image (msg):
-    print ("imagen recibida")
-    bridge = CvBridge()
-    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-    gradients_laplacian = cv2.Laplacian(cv_image, -1)
+    # Print ("imagen recibida")
+    bridge = CvBridge() 
+    # Read de original image 
+    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')#source file
+    # Convert to hls space color: brightness > color intensity
+    hls_image= cv2.cvtColor(cv_image, cv2.COLOR_BGR2HLS)
+    Lchannel=hls_image[:,:,2]#To access the entire lightness channel
+    mask = cv2.inRange(Lchannel, 0,0)#Create the mask
+    image_mask=cv2.bitwise_and(cv_image,cv_image,mask=mask)#Apply mask to original image
+    
+    
+    #--------------------------FIND EDGES*------------------------------------#
+    # Detect lines using Probabilistic Hough Line Transform
+    # Blur the image for better edge detection
+    blur=cv2.GaussianBlur(image_mask,(7,7),0)
+    # Canny Edge Detection
+    edges = cv2.Canny(blur,120, 150) # Canny Edge Detection
+    cdstP = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR) 
+    lines = cv2.HoughLinesP(edges, 1, numpy.pi/180, threshold=50, minLineLength=10, maxLineGap=60)
+    #for the method above https://docs.opencv.org/3.4/d9/db0/tutorial_hough_lines.html
+    # Draw lines
+    if lines is not None:
+        for i in range(0, len(lines)):
+            l = lines[i][0]#retrieves cordinates
+            cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 2, cv2.LINE_AA)#draws a line onto the image, connecting the endpoints
 
-    canny_output = cv2.Canny(cv_image, 80, 150)
-    cv2.imshow('Canny', canny_output)
+    #-------------------------FIND CONTOURS**------------------------------#
+    #apply binary thresholding
+    gray = cv2.cvtColor(cdstP, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 50, 150, cv2.THRESH_BINARY)#0 is black 255 is white
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    #RETR_TREE ----> retrieves all contours with hierarchy 
+    #RETR_LIST ----> retrieves all contours without hierarchy
+    #RETR_EXTERNAL --->retrieves only external contours
+    #CHAIN_APPROX_NONE---> stores all the contour points
+    #CHAIN_APPROX_SIMPLE---> compresses h,v,d segments only their points
+    #contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.INTERSECT_FULL, method=cv2.CHAIN_APPROX_NONE)
+    #-----------draw contours on the original image--------------
+    contour_image = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(image=contour_image, contours=contours, contourIdx=-1, color=(0, 0, 250), thickness=2, lineType=cv2.LINE_AA)
+    #print("Number of contours found:", len(contours)) 
+    target_contour=None
 
-    cv2.imshow("imagen",cv_image)
+    for contour in contours:
+        M=cv2.moments(contour)
+        hu_moments = cv2.HuMoments(M)        
+        if 0.21<hu_moments[0]<0.24 and 0.01<hu_moments[1]<0.018 and hu_moments[5]>0 and hu_moments[6]>0.000001:
+            i+=1
+            real_contours=[i]
+            target_contour=contour           
+            #shorter x longer y /// longer x y shorter y
+            break
+    global cx,cy
+    if target_contour is not None:
+        #M_t = cv2.moments(target_contour)
+        #hu_moments = cv2.HuMoments(M_t) 
+        cx=int(M["m10"]/M["m00"])
+        cy=int(M["m01"]/M["m00"])        
+        print("Centroid:", (cx,cy)) 
+        
+        print("Target contour:", len(target_contour)) 
+        print("Number of contours found:", len(real_contours))
+        print("Hu moments:",hu_moments)
+        cv2.drawContours(image=contour_image, contours=[target_contour], contourIdx=-1, color=(255, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+        error_x = cx-320
+        error_y = cy - 240
+        goal_pan = -1/10000.0 * error_x
+        goal_tilt = 1/7000.0 * error_y
+
+        if  goal_pan > 1.5:
+             goal_pan = 1.5
+
+        if goal_pan < -1.5:
+           goal_pan = -1.5
+
+        if goal_tilt > 1:
+           goal_tilt = 1
+
+        if goal_tilt < -1:
+           goal_tilt = -1
+        contour_image=cv2.circle(contour_image, (cx,cy), radius=0, color=(0, 255, 0), thickness=-1)
+    # head_goal_pose = Float32MultiArray()
+    # head_goal_pose.data = [goal_pan, goal_tilt]
+    # print(head_goal_pose.data)
+    # pub_head_goal.publish(head_goal_pose)
+
+        centroid_pub_msg = Point32()
+        centroid_pub_msg.x=cx
+        centroid_pub_msg.y=cy
+        centroid_pub.publish(centroid_pub_msg)
+        # Convert Hu moments to a list for easier comparison
+
+    #print(f"Area of contour: {area,hu_moments}")
+    # Detect lines using Hough Line Transform
+    #---------------------------Head movement----------------------------------#
+    #pub_contours.publish(bridge.cv2_to_imgmsg(contour_image, encoding = "rgb8"))
+    # see the results
+    #cv2.imshow('None approximation', image_copy)
+    #cv2.waitKey(10)
+    #cv2.imwrite('contours_none_image1.jpg', image_copy)
+    # Display images
+    #Transform img to message with cv_bridge
+
+    #cv2.imshow("imagen",cv_image) #source file
+    msg = bridge.cv2_to_imgmsg(cv_image, encoding='rgb8')
+    original_cv_image_pub.publish(msg)
+
+    cv2.imshow('HLS image', hls_image) #hls image
+    cv2.imshow("blur image",blur) #Blur image
+    cv2.imshow("image mask",image_mask) #Masked image
+    cv2.imshow('Canny Edge Detection', edges)#*Canny edge detection
+    cv2.imshow("Contours", contour_image)#contours found
+    cv2.imshow("Detected Lines-Probabilistic Line Transform", cdstP)
     cv2.waitKey(10)
 
 
 def main ():
+    global pub_head_goal,centroid_pub, original_cv_image_pub
+    pub_head_goal = rospy.Publisher("/hardware/head_goal_pose", Float32MultiArray, queue_size=1)
+    centroid_pub = rospy.Publisher("/centroid_publisher", Point32, queue_size=1)
+    original_cv_image_pub = rospy.Publisher("/original_image", Image, queue_size=1)
     rospy.init_node("goal_detection_node")  
     rospy.Subscriber("/hardware/camera/image", Image, callback_image)
     rospy.spin()
--- catkin_ws/src/vision/goal_detection/scripts/head_goal_ball_position_node.py
+++ catkin_ws/src/vision/goal_detection/scripts/head_goal_ball_position_node.py
@@ -12,69 +12,61 @@ from cv_bridge import CvBridge
 from matplotlib import pyplot as plt
 from geometry_msgs.msg import Point32
 from std_msgs.msg import Float32MultiArray
-
-new_head_pose=[0.00,0.00]
-
 def head_k_generator(centroid_msg):
-    rate=rospy.Rate(4)
-    ball_found=Bool
-    global new_head_pose
     center_x=centroid_msg.x
     center_y=centroid_msg.y
+    head_cmd = Float32MultiArray()
     Cx=320
     Cy=240
     ex=center_x-Cx
     ey=center_y-Cy
-    Kpan=-0.07/320
-    Ktilt=0.07/240
-    center_x0=320
-    center_y0=240
-    if center_x==0 and center_y==0:
-        for i in range(10):
-            center_x0+=20
-            if 0<center_x0<521:
-                print(f"Looking for a ball... in {center_x0}")
-                ex0=center_x0-Cx
-                ey0=center_y0-Cy
-                pan = (ex0*Kpan)
-                tilt = (ey0*Ktilt)
-            if center_x0>=520:
-                print("Returning...")
-                ex0=center_x0-Cx
-                ey0=center_y0-Cy
-                pan = (ex0*Kpan)*-1
-                tilt = (ey0*Ktilt)
-
-            new_head_pose = [sum(x) for x in zip(new_head_pose, [pan, tilt])]
-            head_cmd = Float32MultiArray()
-            goal=new_head_pose
+    Kpan=0.5/320
+    Ktilt=0.25/240
+    while center_x!=0 and center_y!=0:
+        if ex > 0 and ey < 0: #Ball is up right
+            pan=(Kpan*ex)
+            tilt=(Ktilt*ey)
+            print ("Ball is up right : ",[pan,tilt])
+            print ("Center is: ",[center_x,center_y])
+            goal=[pan,tilt]
             head_cmd.data=goal
             pub_head_goal.publish(head_cmd) 
-            rate.sleep()
-    else:
-        pan = (ex*Kpan)
-        tilt = (ey*Ktilt)
-        print(f"Error ex is {ex}")
-        print(f"Error ey is {ey}")
-        print(f"Moving pan angle by {pan} radians")
-        print(f"Moving tilt angle by {tilt} radians")
+        elif ex > 0 and ey > 0: #Ball is down right
+            pan=(Kpan*ex)
+            tilt=(Ktilt*ey)
+            print ("Ball is down right : ",[pan,tilt])
+            print ("Center is: ",[center_x,center_y])
+            goal=[pan,tilt]
+            head_cmd.data=goal
+            pub_head_goal.publish(head_cmd) 
+        elif ex < 0 and ey < 0: #Ball up left
+            pan=(Kpan*ex)
+            tilt=(Ktilt*ey)
+            print ("Ball is up left : ",[pan,tilt])
+            print ("Center is: ",[center_x,center_y])
+            goal=[pan,tilt]
+            head_cmd.data=goal
+            pub_head_goal.publish(head_cmd)
+        elif ex < 0 and ey > 0: #Ball is down left
+            pan=(Kpan*ex)
+            tilt=(Ktilt*ey)
+            print ("Ball is down left : ",[pan,tilt])
+            print ("Center is: ",[center_x,center_y])
+            goal=[pan,tilt]
+            head_cmd.data=goal
+            pub_head_goal.publish(head_cmd) 
+        elif 315 <= center_x <= 325 and 235 <= center_y <= 245:
+            print ("Ball is centering : ",[center_x,center_y]) 
 
-        new_head_pose = [sum(x) for x in zip(new_head_pose, [pan, tilt])]
-    
-    print(new_head_pose)
-    head_cmd = Float32MultiArray()
-    goal=new_head_pose
-    head_cmd.data=goal
-    pub_head_goal.publish(head_cmd) 
-    
-
+        else:
+            print("Any ball detected")
+        break        
+        #return pan,tilt
 def main ():
-    global pub_head_goal,ball_found_pub
-    rospy.init_node("head_goal_ball_position_node")    
+    global pub_head_goal,rate
+    rospy.init_node("head_goal_ball_position_node")  
     rospy.Subscriber("/centroid_publisher", Point32, head_k_generator)
-    pub_head_goal = rospy.Publisher("/hardware/head_goal_pose", Float32MultiArray, queue_size=1)  
-    ball_found_pub = rospy.Publisher("/ball_found", Bool, queue_size=1)
- 
+    pub_head_goal = rospy.Publisher("/head_goal_pose", Float32MultiArray, queue_size=1)   
     rospy.spin()
 
 if __name__=="__main__":
