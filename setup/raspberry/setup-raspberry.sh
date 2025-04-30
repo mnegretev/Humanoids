569a8db77c55541f2c7064267b85860a323e7f66
Miguel Garcia <garcia.miguel.onate@gmail.com>
2025-04-30 01:56:08 -0600

    udev rules added

--- setup/raspberry/setup-raspberry.sh
+++ setup/raspberry/setup-raspberry.sh
@@ -1,3 +1,11 @@
+set -e
+
 sudo apt-get remove -y unattended-upgrades
 sudo apt-get install i2c-tools
+
+sudo cp $(git rev-parse --show-toplevel)/setup/raspberry/80-humanoid.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && sudo service udev restart && sudo udevadm trigger
+
 echo "source ~/Humanoids/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
+echo "Finished setting up raspberry"
