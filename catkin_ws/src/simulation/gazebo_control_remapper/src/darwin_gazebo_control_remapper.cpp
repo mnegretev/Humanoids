2ce24b16e9a099f6e7e896e13964a9c7ab44b666
Miguel Garcia <garcia.miguel.onate@gmail.com>
2025-05-02 02:42:03 -0600

    DARWIN WORKS. CLEANUP REPOSITORY. DELETED USELESS PACKAGES :bangbang:

---
+++ catkin_ws/src/simulation/gazebo_control_remapper/src/darwin_gazebo_control_remapper.cpp
@@ -0,0 +1,230 @@
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+#include "std_msgs/Float32MultiArray.h"
+
+ros::Publisher pubLeftHipYaw        ;
+ros::Publisher pubLeftHipRoll       ;
+ros::Publisher pubLeftHipPitch      ;
+ros::Publisher pubLeftKneePitch     ;
+ros::Publisher pubLeftAnklePitch    ;
+ros::Publisher pubLeftAnkleRoll     ;
+ros::Publisher pubRightHipYaw       ;
+ros::Publisher pubRightHipRoll      ;
+ros::Publisher pubRightHipPitch     ;
+ros::Publisher pubRightKneePitch    ;
+ros::Publisher pubRightAnklePitch   ;
+ros::Publisher pubRightAnkleRoll    ;
+ros::Publisher pubLeftShoulderPitch ;
+ros::Publisher pubLeftShoulderRoll  ;
+ros::Publisher pubLeftElbowPitch    ;
+ros::Publisher pubRightShoulderPitch;
+ros::Publisher pubRightShoulderRoll ;
+ros::Publisher pubRightElbowPitch   ;
+ros::Publisher pubNeckYaw           ;
+ros::Publisher pubNeckPitch         ;
+std_msgs::Float64 msgLeftHipYaw        ;
+std_msgs::Float64 msgLeftHipRoll       ;
+std_msgs::Float64 msgLeftHipPitch      ;
+std_msgs::Float64 msgLeftKneePitch     ;
+std_msgs::Float64 msgLeftAnklePitch    ;
+std_msgs::Float64 msgLeftAnkleRoll     ;
+std_msgs::Float64 msgRightHipYaw       ;
+std_msgs::Float64 msgRightHipRoll      ;
+std_msgs::Float64 msgRightHipPitch     ;
+std_msgs::Float64 msgRightKneePitch    ;
+std_msgs::Float64 msgRightAnklePitch   ;
+std_msgs::Float64 msgRightAnkleRoll    ;
+std_msgs::Float64 msgLeftShoulderPitch ;
+std_msgs::Float64 msgLeftShoulderRoll  ;
+std_msgs::Float64 msgLeftElbowPitch    ;
+std_msgs::Float64 msgRightShoulderPitch;
+std_msgs::Float64 msgRightShoulderRoll ;
+std_msgs::Float64 msgRightElbowPitch   ;
+std_msgs::Float64 msgNeckYaw           ;
+std_msgs::Float64 msgNeckPitch         ;
+
+void callback_legs_goal_pose(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+    if(msg->data.size() != 12)
+    {
+	std::cout << "ControlRemapper.->Error!!: goal position for both legs must be a 12-value array." << std::endl;
+	return;
+    }
+    msgLeftHipYaw       .data = msg->data[0];
+    msgLeftHipRoll      .data = msg->data[1];
+    msgLeftHipPitch     .data = msg->data[2];
+    msgLeftKneePitch    .data = msg->data[3];
+    msgLeftAnklePitch   .data = msg->data[4];
+    msgLeftAnkleRoll    .data = msg->data[5];
+    msgRightHipYaw      .data = msg->data[6];
+    msgRightHipRoll     .data = msg->data[7];
+    msgRightHipPitch    .data = msg->data[8];
+    msgRightKneePitch   .data = msg->data[9];
+    msgRightAnklePitch  .data = msg->data[10];
+    msgRightAnkleRoll   .data = msg->data[11];
+
+    pubLeftHipYaw        .publish(msgLeftHipYaw        );
+    pubLeftHipRoll       .publish(msgLeftHipRoll       );
+    pubLeftHipPitch      .publish(msgLeftHipPitch      );
+    pubLeftKneePitch     .publish(msgLeftKneePitch     );
+    pubLeftAnklePitch    .publish(msgLeftAnklePitch    );
+    pubLeftAnkleRoll     .publish(msgLeftAnkleRoll     );
+    pubRightHipYaw       .publish(msgRightHipYaw       );
+    pubRightHipRoll      .publish(msgRightHipRoll      );
+    pubRightHipPitch     .publish(msgRightHipPitch     );
+    pubRightKneePitch    .publish(msgRightKneePitch    );
+    pubRightAnklePitch   .publish(msgRightAnklePitch   );
+    pubRightAnkleRoll    .publish(msgRightAnkleRoll    );
+}
+
+void callback_leg_left_goal_pose(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+    if(msg->data.size() != 6)
+    {
+	std::cout << "ControlRemapper.->Error!!: goal position for left leg must be a 6-value array." << std::endl;
+	return;
+    }
+    msgLeftHipYaw     .data = msg->data[0];
+    msgLeftHipRoll    .data = msg->data[1];
+    msgLeftHipPitch   .data = msg->data[2];
+    msgLeftKneePitch  .data = msg->data[3];
+    msgLeftAnklePitch .data = msg->data[4];
+    msgLeftAnkleRoll  .data = msg->data[5];
+    pubLeftHipYaw        .publish(msgLeftHipYaw        );
+    pubLeftHipRoll       .publish(msgLeftHipRoll       );
+    pubLeftHipPitch      .publish(msgLeftHipPitch      );
+    pubLeftKneePitch     .publish(msgLeftKneePitch     );
+    pubLeftAnklePitch    .publish(msgLeftAnklePitch    );
+    pubLeftAnkleRoll     .publish(msgLeftAnkleRoll     );
+}
+
+void callback_leg_right_goal_pose(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+    if(msg->data.size() != 6)
+    {
+	std::cout << "ControlRemapper.->Error!!: goal position for right leg must be a 6-value array." << std::endl;
+	return;
+    }
+    msgRightHipYaw    .data = msg->data[0];
+    msgRightHipRoll   .data = msg->data[1];
+    msgRightHipPitch  .data = msg->data[2];
+    msgRightKneePitch .data = msg->data[3];
+    msgRightAnklePitch.data = msg->data[4];
+    msgRightAnkleRoll .data = msg->data[5];
+    pubRightHipYaw       .publish(msgRightHipYaw       );
+    pubRightHipRoll      .publish(msgRightHipRoll      );
+    pubRightHipPitch     .publish(msgRightHipPitch     );
+    pubRightKneePitch    .publish(msgRightKneePitch    );
+    pubRightAnklePitch   .publish(msgRightAnklePitch   );
+    pubRightAnkleRoll    .publish(msgRightAnkleRoll    );
+}
+
+void callback_arms_goal_pose(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+    if(msg->data.size() != 6)
+    {
+	std::cout << "ControlRemapper.->Error!!: goal position for both arms must be a 6-value array." << std::endl;
+	return;
+    }
+
+    msgLeftShoulderPitch .data = msg->data[0];
+    msgLeftShoulderRoll  .data = msg->data[1];
+    msgLeftElbowPitch    .data = msg->data[2];
+    msgRightShoulderPitch.data = msg->data[3];
+    msgRightShoulderRoll .data = msg->data[4];
+    msgRightElbowPitch   .data = msg->data[5];
+    pubLeftShoulderPitch .publish(msgLeftShoulderPitch );
+    pubLeftShoulderRoll  .publish(msgLeftShoulderRoll  );
+    pubLeftElbowPitch    .publish(msgLeftElbowPitch    );
+    pubRightShoulderPitch.publish(msgRightShoulderPitch);
+    pubRightShoulderRoll .publish(msgRightShoulderRoll );
+    pubRightElbowPitch   .publish(msgRightElbowPitch   );
+}
+
+void callback_arm_left_goal_pose(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+    if(msg->data.size() != 3)
+    {
+	std::cout << "ControlRemapper.->Error!!: goal position for left arm must be a 3-value array." << std::endl;
+	return;
+    }
+    msgLeftShoulderPitch.data = msg->data[0];
+    msgLeftShoulderRoll .data = msg->data[1];
+    msgLeftElbowPitch   .data = msg->data[2];
+    pubLeftShoulderPitch .publish(msgLeftShoulderPitch );
+    pubLeftShoulderRoll  .publish(msgLeftShoulderRoll  );
+    pubLeftElbowPitch    .publish(msgLeftElbowPitch    );
+}
+
+void callback_arm_right_goal_pose(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+    if(msg->data.size() != 3)
+    {
+	std::cout << "ControlRemapper.->Error!!: goal position for right leg must be a 3-value array." << std::endl;
+	return;
+    }
+    msgRightShoulderPitch.data = msg->data[0];
+    msgRightShoulderRoll .data = msg->data[1];
+    msgRightElbowPitch   .data = msg->data[2];
+    pubRightShoulderPitch.publish(msgRightShoulderPitch);
+    pubRightShoulderRoll .publish(msgRightShoulderRoll );
+    pubRightElbowPitch   .publish(msgRightElbowPitch   );
+}
+
+void callback_head_goal_pose(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+    if(msg->data.size() != 2)
+    {
+	std::cout << "ControlRemapper.->Error!! goal position for head must be a 2-value array" << std::endl;
+	return;
+    }
+    msgNeckYaw  .data = msg->data[0];
+    msgNeckPitch.data = msg->data[1];
+    pubNeckYaw           .publish(msgNeckYaw           );
+    pubNeckPitch         .publish(msgNeckPitch         );
+}
+
+
+int main(int argc, char** argv)
+{
+    std::cout << "INITIALIZING DARWIN GAZEBO CONTROL MAPPER BY MARCOSOFT..." << std::endl;
+    ros::init(argc, argv, "darwin_gazebo_control_remapper");
+    ros::NodeHandle n;
+    ros::Rate loop(30);
+
+    ros::Subscriber sub_legs_goal_pose      = n.subscribe("legs_goal_pose", 1, callback_legs_goal_pose);
+    ros::Subscriber sub_leg_left_goal_pose  = n.subscribe("leg_left_goal_pose", 1, callback_leg_left_goal_pose);
+    ros::Subscriber sub_leg_right_goal_pose = n.subscribe("leg_right_goal_pose", 1, callback_leg_right_goal_pose);
+    ros::Subscriber sub_arms_goal_pose      = n.subscribe("arms_goal_pose", 1, callback_arms_goal_pose);
+    ros::Subscriber sub_arm_left_goal_pose  = n.subscribe("arm_left_goal_pose", 1, callback_arm_left_goal_pose);
+    ros::Subscriber sub_arm_right_goal_pose = n.subscribe("arm_right_goal_pose", 1, callback_arm_right_goal_pose);
+    ros::Subscriber sub_head_goal_pose      = n.subscribe("head_goal_pose", 1, callback_head_goal_pose);
+
+    pubLeftHipYaw         = n.advertise<std_msgs::Float64>("/darwin/left_hip_yaw_position_controller/command",1);
+    pubLeftHipRoll        = n.advertise<std_msgs::Float64>("/darwin/left_hip_roll_position_controller/command",1);
+    pubLeftHipPitch       = n.advertise<std_msgs::Float64>("/darwin/left_hip_pitch_position_controller/command",1);
+    pubLeftKneePitch      = n.advertise<std_msgs::Float64>("/darwin/left_knee_pitch_position_controller/command",1);
+    pubLeftAnklePitch     = n.advertise<std_msgs::Float64>("/darwin/left_ankle_pitch_position_controller/command",1);
+    pubLeftAnkleRoll      = n.advertise<std_msgs::Float64>("/darwin/left_ankle_roll_position_controller/command",1); 
+    pubRightHipYaw        = n.advertise<std_msgs::Float64>("/darwin/right_hip_yaw_position_controller/command",1);    
+    pubRightHipRoll       = n.advertise<std_msgs::Float64>("/darwin/right_hip_roll_position_controller/command",1);   
+    pubRightHipPitch      = n.advertise<std_msgs::Float64>("/darwin/right_hip_pitch_position_controller/command",1);  
+    pubRightKneePitch     = n.advertise<std_msgs::Float64>("/darwin/right_knee_pitch_position_controller/command",1); 
+    pubRightAnklePitch    = n.advertise<std_msgs::Float64>("/darwin/right_ankle_pitch_position_controller/command",1);
+    pubRightAnkleRoll     = n.advertise<std_msgs::Float64>("/darwin/right_ankle_roll_position_controller/command",1);
+    pubLeftShoulderPitch  = n.advertise<std_msgs::Float64>("/darwin/left_shoulder_pitch_position_controller/command", 1);
+    pubLeftShoulderRoll   = n.advertise<std_msgs::Float64>("/darwin/left_shoulder_roll_position_controller/command",1);  
+    pubLeftElbowPitch     = n.advertise<std_msgs::Float64>("/darwin/left_elbow_pitch_position_controller/command", 1); 
+    pubRightShoulderPitch = n.advertise<std_msgs::Float64>("/darwin/right_shoulder_pitch_position_controller/command", 1);
+    pubRightShoulderRoll  = n.advertise<std_msgs::Float64>("/darwin/right_shoulder_roll_position_controller/command", 1);
+    pubRightElbowPitch    = n.advertise<std_msgs::Float64>("/darwin/right_elbow_pitch_position_controller/command", 1);
+    pubNeckYaw            = n.advertise<std_msgs::Float64>("/darwin/neck_yaw_position_controller/command", 1);
+    pubNeckPitch          = n.advertise<std_msgs::Float64>("/darwin/head_pitch_position_controller/command", 1);
+
+    while(ros::ok())
+    {
+	ros::spinOnce();
+	ros::spinOnce();
+	loop.sleep();
+    }
+}
---
+++ catkin_ws/src/simulation/gazebo_joint_reader/src/darwin_gazebo_joint_reader.cpp
@@ -0,0 +1,94 @@
+#include "ros/ros.h"
+#include "std_msgs/Float32MultiArray.h"
+#include "sensor_msgs/JointState.h"
+#include "gazebo_msgs/GetJointProperties.h"
+#include "gazebo_msgs/GetLinkState.h"
+#include "tf/transform_broadcaster.h"
+
+int main(int argc, char** argv)
+{
+    std::cout << "INITIALIZING GAZEBO JOINT READER BY MARCOSOFT..." << std::endl;
+    ros::init(argc, argv, "darwin_gazebo_joint_reader");
+    ros::NodeHandle n;
+    ros::Rate loop(30);
+
+    ros::Publisher pub_legs_current_pose      = n.advertise<std_msgs::Float32MultiArray>("legs_current_pose", 1);
+    ros::Publisher pub_leg_left_current_pose  = n.advertise<std_msgs::Float32MultiArray>("leg_left_current_pose", 1);
+    ros::Publisher pub_leg_right_current_pose = n.advertise<std_msgs::Float32MultiArray>("leg_right_current_pose", 1);
+    ros::Publisher pub_arms_current_pose      = n.advertise<std_msgs::Float32MultiArray>("arms_current_pose", 1);
+    ros::Publisher pub_arm_left_current_pose  = n.advertise<std_msgs::Float32MultiArray>("arm_left_current_pose", 1);
+    ros::Publisher pub_arm_right_current_pose = n.advertise<std_msgs::Float32MultiArray>("arm_right_current_pose", 1);
+    ros::Publisher pub_head_current_pose      = n.advertise<std_msgs::Float32MultiArray>("head_current_pose", 1);
+    ros::Publisher pub_joint_current_angles   = n.advertise<std_msgs::Float32MultiArray>("joint_current_angles", 1);
+    std_msgs::Float32MultiArray msg_joint_current_angles;
+    msg_joint_current_angles.data.resize(20);
+    ros::Duration(0.1).sleep();
+    ros::service::waitForService("/gazebo/get_joint_properties", ros::Duration(10));
+    ros::service::waitForService("/gazebo/get_link_state", ros::Duration(10));
+    ros::ServiceClient clt_joints       = n.serviceClient<gazebo_msgs::GetJointProperties>("/gazebo/get_joint_properties");
+    ros::ServiceClient clt_trunk_state  = n.serviceClient<gazebo_msgs::GetLinkState>("/gazebo/get_link_state");
+    gazebo_msgs::GetJointProperties srv_joints;
+    gazebo_msgs::GetLinkState       srv_trunk_state;
+    srv_trunk_state.request.link_name = "darwin_lab::base_link";
+
+    ros::Publisher  pub_joint_states = n.advertise<sensor_msgs::JointState>("/joint_states", 1);
+    sensor_msgs::JointState joint_states;
+
+    joint_states.name.resize(20);
+    joint_states.position.resize(20);
+
+    joint_states.name[0]  ="left_hip_yaw";   
+    joint_states.name[1]  ="left_hip_roll";  
+    joint_states.name[2]  ="left_hip_pitch"; 
+    joint_states.name[3]  ="left_knee_pitch";
+    joint_states.name[4]  ="left_ankle_pitch";
+    joint_states.name[5]  ="left_ankle_roll";
+  
+    joint_states.name[6]  ="right_hip_yaw";
+    joint_states.name[7]  ="right_hip_roll";
+    joint_states.name[8]  ="right_hip_pitch";
+    joint_states.name[9]  ="right_knee_pitch";
+    joint_states.name[10] ="right_ankle_pitch";
+    joint_states.name[11] ="right_ankle_roll";
+
+    joint_states.name[12] ="left_shoulder_pitch";
+    joint_states.name[13] ="left_shoulder_roll";
+    joint_states.name[14] ="left_elbow_pitch";
+
+    joint_states.name[15] ="right_shoulder_pitch";
+    joint_states.name[16] ="right_shoulder_roll";
+    joint_states.name[17] ="right_elbow_pitch";
+
+    joint_states.name[18] ="neck_yaw";
+    joint_states.name[19] ="head_pitch";
+
+    tf::TransformBroadcaster br;
+    tf::Transform t;
+
+    while(ros::ok())
+    {
+        joint_states.header.stamp = ros::Time::now();
+        for(size_t i=0; i < joint_states.name.size(); i++)
+        {
+            srv_joints.request.joint_name = joint_states.name[i];
+            clt_joints.call(srv_joints);
+            joint_states.position[i] = fmod(srv_joints.response.position[0] + M_PI, 2*M_PI) - M_PI;
+            msg_joint_current_angles.data[i] = joint_states.position[i];
+        }
+
+        clt_trunk_state.call(srv_trunk_state);
+        t.setOrigin(tf::Vector3(srv_trunk_state.response.link_state.pose.position.x,
+                                srv_trunk_state.response.link_state.pose.position.y,
+                                srv_trunk_state.response.link_state.pose.position.z));
+        t.setRotation(tf::Quaternion(srv_trunk_state.response.link_state.pose.orientation.x,
+                                     srv_trunk_state.response.link_state.pose.orientation.y,
+                                     srv_trunk_state.response.link_state.pose.orientation.z,
+                                     srv_trunk_state.response.link_state.pose.orientation.w));
+
+        br.sendTransform(tf::StampedTransform(t, ros::Time::now(), "map", "base_link"));
+        pub_joint_states.publish(joint_states);
+        pub_joint_current_angles.publish(msg_joint_current_angles);
+        ros::spinOnce();
+        loop.sleep();
+    }
+}
