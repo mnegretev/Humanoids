6a84aa21d2661835414a0ff5a29330f6b7d54d11
Miguel Garcia <garcia.miguel.onate@gmail.com>
2025-06-12 13:32:26 -0600

    imu updates

--- catkin_ws/src/hardware/imu/scripts/imu_handler_node.py
+++ catkin_ws/src/hardware/imu/scripts/imu_handler_node.py
@@ -3,6 +3,8 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Vector3
+from tf import TransformBroadcaster
+from tf.transformations import quaternion_from_euler
 import math
 
 FREQ=25
@@ -12,11 +14,14 @@ class Imu_Node():
         self.sub    = rospy.Subscriber("/imu/data_raw", Imu, self.callback)
         self.pub    = rospy.Publisher("/imu/humanoid_orientation", Vector3, queue_size=1)
         self.pub_hs = rospy.Publisher("/imu/state", String, queue_size=1)
-        self.pub_msg    = Vector3()
+        self.b      = TransformBroadcaster()
+        self.pub_msg= Vector3()
         self.rate   = rospy.Rate(5)
         self.gx     = 0
         self.gy     = 0
         self.gz     = 0
+        self.translation    = (0.0, 0.0, 0.0)
+        self.rotation       = None#(0.0, 0.0, 0.0, 1.0)
 
     def callback(self, msg):
         ay     = math.atan2(msg.linear_acceleration.x, math.sqrt( msg.linear_acceleration.y**2 + msg.linear_acceleration.z**2)) * 180 / math.pi
@@ -29,9 +34,9 @@ class Imu_Node():
         self.gx = self.gx * 0.96 + ax * 0.04
         self.gy = self.gy * 0.96 + ay * 0.04
 
-        self.pub_msg.x = self.gx
-        self.pub_msg.y = self.gy
-        self.pub_msg.z = self.gz
+        self.rotation = quaternion_from_euler(self.gx, self.gy, self.gz)
+
+        self.b.sendTransform(self.translation, self.rotation, rospy.Time.now(), 'imu_link_rotation', 'imu_link')
 
         self.pub.publish(self.pub_msg)
         
