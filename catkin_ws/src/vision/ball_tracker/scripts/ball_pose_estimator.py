f5665ac41470a862c286d91b4ebc822ed9b4e624
FriasCamille <Roman@cualquier.com>
2025-06-12 19:24:54 -0600

    functional server for ball from Miriam

---
+++ catkin_ws/src/vision/ball_tracker/scripts/ball_pose_estimator.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+#import tf2_ros
+import tf
+
+from geometry_msgs.msg import Point, PointStamped
+from sensor_msgs.msg import Image
+from vision_msgs.srv import ProcessObject, ProcessObjectResponse
+from vision_msgs.msg import VisionObject
+
+image_width = 1280
+image_height = 720
+center_x = image_width / 2
+center_y = image_height / 2
+HFOV = math.radians(90.0)
+VFOV = math.radians(60.0)
+
+def callback_calculate_position(req):
+    #global tf_buffer
+
+    x_B = req.object.x
+    y_B = req.object.y
+
+
+    theta = (VFOV / image_height) * (center_y - y_B)
+    phi = (HFOV / image_width) * (center_x - x_B)
+
+    ux = math.cos(theta) * math.cos(phi)
+    uy = math.cos(theta) * math.sin(phi)
+    uz = math.sin(theta)
+    print("wrt_camera", ux, uy, uz)
+
+    # try:
+    #     trans = tf_buffer.lookup_transform('left_foot_link', 'camera_optical', rospy.Time(0), rospy.Duration(1.0))
+    # except Exception as e:
+    #     rospy.logwarn(f"Error al obtener la transformación tf: {e}")
+    #     return ProcessObjectResponse()
+
+    p = PointStamped()
+    p.header.frame_id = 'camera_optical'
+    p.header.stamp = rospy.Time(0)
+    p.point.x, p.point.y, p.point.z = ux, uy, uz
+    listener.waitForTransform('left_foot_link','camera_optical', rospy.Time(), rospy.Duration(10.0))
+    p = listener.transformPoint('left_foot_link', p)
+    #p = tf_buffer.transform(p, 'left_foot_link')
+    print("wrt_foot", p)
+    camera = PointStamped()
+    camera.header.frame_id = 'camera_optical'
+    camera.header.stamp = rospy.Time(0)
+    camera = listener.transformPoint('left_foot_link', camera)
+
+    print("camera", camera)
+
+    lx, ly, lz = p.point.x - camera.point.x, p.point.y - camera.point.y, p.point.z - camera.point.z
+    mag = math.sqrt(lx * lx + ly * ly + lz * lz)
+    lx, ly, lz = lx / mag, ly / mag, lz / mag
+    print("l", lx, ly, lz)
+    d = (-camera.point.z / (lz))
+    print("d", d)
+    px, py, pz = camera.point.x + d * lx, camera.point.y + d * ly, camera.point.z + d * lz
+    ball = PointStamped()
+    ball.header.frame_id = 'left_foot_link'
+    ball.header.stamp = rospy.Time(0)
+    ball.point.x, ball.point.y, ball.point.z = px, py, pz 
+    print(px, py, pz)
+    #r = trans.transform.translation
+    #print(r)
+    #r_x, r_y, r_z = r.x, r.y, r.z
+
+
+    #tan_theta = math.tan(theta)
+    #est_x = r_x - r_z * tan_theta * math.cos(phi)
+    #est_y = r_y - r_z * tan_theta * math.sin(phi)
+
+
+    res = ProcessObjectResponse()
+    res.object = req.object
+    res.object.pose.position.x = px
+    res.object.pose.position.y = py
+    res.object.pose.position.z = pz
+    #res.img = req.img
+
+    pub.publish(ball)
+
+    return res
+
+def ball_pose_estimator():
+    #global tf_buffer
+    global listener
+    global pub
+    rospy.init_node('ball_pose_estimator')
+
+    #tf_buffer = tf2_ros.Buffer()
+    listener = tf.TransformListener()
+
+    s = rospy.Service('/vision/img_to_cartesian_floor', ProcessObject, callback_calculate_position)
+    rospy.loginfo("Servicio 'process_object' listo.")
+    pub = rospy.Publisher("ball_position", PointStamped, queue_size= 1)
+    rospy.spin()
+
+if __name__ == "__main__":
+
+    ball_pose_estimator()
--- catkin_ws/src/vision/luckfox_vision/src/luckfox_client_node.cpp
+++ catkin_ws/src/vision/luckfox_vision/src/luckfox_client_node.cpp
@@ -2,7 +2,7 @@
 #include "vision_msgs/ProcessObject.h"
 #include <cstdlib>
 
-//socket libs
+// socket libs
 
 #include <iostream>
 #include <cstring>
@@ -13,25 +13,24 @@
 #include <vector>
 #define PORT 5000
 
-
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "Luckfox_node");
 
-    ros::NodeHandle n; 
-    ros::ServiceClient client = n.serviceClient<vision_msgs::ProcessObject>("ProcessObjects");
+    ros::NodeHandle n;
+    ros::ServiceClient client = n.serviceClient<vision_msgs::ProcessObject>("/vision/img_to_cartesian_floor");
     vision_msgs::ProcessObject srv;
 
-    if (argc !=3)
+    if (argc != 3)
     {
         ROS_INFO("Reading UDP from the luckfox");
-        //return 1;
+        // return 1;
     }
     ros::Rate loop_rate(10);
 
-    //Cración de socket
+    // Cración de socket
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1) 
+    if (clientSocket == -1)
     {
         std::cerr << "Error al crear el socket del cliente" << std::endl;
         return 1;
@@ -40,11 +39,12 @@ int main(int argc, char **argv)
     // Configurar la dirección del servidor
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(5000); // Puerto 5000
+    serverAddress.sin_port = htons(5000);                     // Puerto 5000
     serverAddress.sin_addr.s_addr = inet_addr("10.42.0.102"); // Dirección local
 
     // Conectar al servidor
-    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+    {
         std::cerr << "Error al conectar con el servidor" << std::endl;
         close(clientSocket);
         return 1;
@@ -52,20 +52,23 @@ int main(int argc, char **argv)
 
     std::cout << "Conectado al servidor. Recibiendo mensajes..." << std::endl;
     std::cout << "Presiona Ctrl+C para detener el cliente." << std::endl;
-    
+
     char buffer[1024] = {0};
     char nombre[100];
     int sX, sY, eX, eY;
     float prop;
+    
+    ros::Publisher pub = n.advertise<vision_msgs::VisionObject>("/vision/landmarks", 1);
 
     while (ros::ok())
-    {    
+    {
         memset(buffer, 0, sizeof(buffer));
 
         // Leer datos del servidor
         int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-        
-        if (bytesRead <= 0) {
+
+        if (bytesRead <= 0)
+        {
             std::cerr << "Conexión cerrada por el servidor o error de lectura" << std::endl;
             break;
         }
@@ -74,33 +77,31 @@ int main(int argc, char **argv)
         std::string message(buffer, bytesRead);
         std::cout << "Mensaje recibido: " << message;
 
-        int elementos_leidos = sscanf(buffer, "%99[^@] @ (%d %d %d %d) %f",nombre, &sX, &sY, &eX, &eY, &prop);
+        int elementos_leidos = sscanf(buffer, "%99[^@] @ (%d %d %d %d) %f", nombre, &sX, &sY, &eX, &eY, &prop);
 
         srv.request.object.id = nombre;
         srv.request.object.confidence = prop;
         srv.request.object.x = sX;
         srv.request.object.y = sY;
-        srv.request.object.width = abs(sX-sY);
-        srv.request.object.height = abs(eX-eY);
-
+        srv.request.object.width = abs(sX - sY);
+        srv.request.object.height = abs(eX - eY);
+        std::cout << srv.request.object.confidence << std::endl;
         if (client.call(srv))
         {
-            
             ROS_INFO("The service worked well");
-            ros::Publisher pub = n.advertise<vision_msgs::VisionObject>("Luckfox_finds", 1);
             vision_msgs::VisionObject msg;
             msg = srv.response.object;
             pub.publish(msg);
-        }   
+        }
         else
         {
             ROS_ERROR("Failed to call service Process objects");
+            // Opcional: puedes imprimir más información de error aquí
+            ROS_ERROR("Service call failed: %s", client.getService().c_str());
             return 1;
         }
         ros::spinOnce();
     }
-    
-    
 
     return 0;
 }
