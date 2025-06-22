e9e4ca62bcbab2d977a7779f317ca5e56d36e65f
AldaySG <galdaysalazar@gmail.com>
2025-06-21 19:46:30 -0600

    Add udp server to luckfox yolo simulation

--- catkin_ws/src/vision/luckfox_vision/scripts/yolo_sim.py
+++ catkin_ws/src/vision/luckfox_vision/scripts/yolo_sim.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python3
 
 import rospy
-#import ros_numpy
 import torch
-#from ultralytics import YOLO
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from socket import socket, AF_INET, SOCK_DGRAM
+from warnings import filterwarnings
+
+filterwarnings("ignore")
+UDP_PORT = 5000
 
 def yolo_callback(msg):
-    global bridge, model, class_image_pub
+    global bridge, model, class_image_pub, udp_server, udp_ip
     img = bridge.imgmsg_to_cv2(msg,"rgb8")
     det_result = model(img)
-    #print(det_result)
-    #print(dir(det_result))
+    print(det_result.pred)
     det_annotated = det_result.render()[0]
-    #print(det_annotated)
+    for det in det_result.xyxy[0]:
+        detarr = det.detach().cpu().numpy()
+        #print(detarr)
+        if len(detarr)>0:
+            vis_str = "{}@({:d},{:d},{:d},{:d}){:.2f}".format(det_result.names[detarr[5]], int(detarr[0]), int(detarr[1]), int(detarr[2]), int(detarr[3]), detarr[4])
+            print(vis_str)
+            udp_server.sendto(vis_str.encode('utf-8'), (udp_ip, UDP_PORT))
     class_image_pub.publish(bridge.cv2_to_imgmsg(det_annotated,"rgb8"))
 
 
 def main():
-    global bridge, model, class_image_pub
+    global bridge, model, class_image_pub, udp_ip, udp_server
     model_path = rospy.get_param("/yolo_model_path")
+    udp_server = socket(AF_INET, SOCK_DGRAM)
     model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
-    #model = torch.hub.load('.', 'custom', path=model_path, source='local') 
+    #model = torch.hub.load('.', 'custom', path=model_path, source='local')
+    rospy.init_node('Yolo_sim')
+    udp_ip = rospy.get_param("~ip_server") 
     class_image_pub = rospy.Publisher("/vision/yolo/image", Image, queue_size=5)
     bridge = CvBridge()
     rospy.Subscriber("/hardware/camera/image", Image, yolo_callback)
-    rospy.init_node('Yolo_sim')
     loop = rospy.Rate(30)
     while not rospy.is_shutdown():
         loop.sleep()
