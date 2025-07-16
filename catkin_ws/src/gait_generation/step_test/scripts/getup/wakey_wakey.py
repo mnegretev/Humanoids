7e51c34f0d8114a9ebc2a4f9c3a715a523857ab3
Miguel Garcia <garcia.miguel.onate@gmail.com>
2025-07-16 14:51:25 +0000

    Finish waking up test + start of fall back get up trajectory

--- catkin_ws/src/gait_generation/step_test/scripts/getup/wakey_wakey.py
+++ catkin_ws/src/gait_generation/step_test/scripts/getup/wakey_wakey.py
@@ -7,16 +7,12 @@ from darwin_gait.srv import WalkGains, WalkGainsRequest
 import numpy as np
 import time
 
-def callback_imu(msg):
-    global humanoid_pose
-    print(msg.data)
-    humanoid_pose = msg.data
 
 def main():
 
     global humanoid_pose
     rospy.init_node("Darwin_lock_in")
-    rospy.Subscriber("/imu/state", String, callback_imu)
+    #rospy.Subscriber("/imu/state", String, callback_imu)
     rospy.wait_for_service("/getup")
     rospy.wait_for_service("/walk_engine")
     get_up_service = rospy.ServiceProxy("/getup", Getup)
@@ -27,24 +23,30 @@ def main():
     req.enabled_gain = 1.0
     req.time = 1.0
 
-    while not rospy.is_shutdown:
+    while not rospy.is_shutdown():
+        print("Entered loop")
+
+        humanoid_pose = rospy.wait_for_message("/imu/state", String, timeout=5).data
 
 
-        while humanoid_pose != "straight": 
+        if humanoid_pose != "straight": 
 
-            if humanoid_pose == "fall_from_front":
+            if humanoid_pose == "fall_front":
                 get_up_service("front")
-            elif humanoid_pose == "fall_from_back":
+            elif humanoid_pose == "fall_back":
                 get_up_service("back")
             else:
                 print("Literally how??????????")
 
             print("Aight its up")
             
-            rospy.sleep(1)
+            rospy.sleep(3)
         
         walk_service(req)
         print("Walking !!!")
+
+if __name__ == '__main__':
+    main()
 
             
 
