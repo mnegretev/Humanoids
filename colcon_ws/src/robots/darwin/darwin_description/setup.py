57e58f4476d398f1d933f8b5adf591d81368e1ae
Miguel Garcia <garcia.miguel.onate@gmail.com>
2025-08-25 21:07:10 -0600

    DARWIN IS ALIVE IN ROS2 BUT NO HEAD :eyes:

---
+++ colcon_ws/src/robots/darwin/darwin_description/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'darwin_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='miguel',
+    maintainer_email='garcia.miguel.onate@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
---
+++ colcon_ws/src/surge_et_ambula/launch/darwin_sim.launch.py
@@ -0,0 +1,76 @@
+from launch.substitutions import PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_prefix
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import xacro
+import os
+
+def generate_launch_description():
+
+    pkg_share_path = os.pathsep + os.path.join(get_package_prefix('darwin_description'), 'share')
+    if 'GZ_SIM_RESOURCE_PATH' in os.environ:
+        os.environ['GZ_SIM_RESOURCE_PATH'] += pkg_share_path
+    else:
+        os.environ['GZ_SIM_RESOURCE_PATH'] =  pkg_share_path
+
+    ros_gz_sim_pkg_path  = get_package_share_directory('ros_gz_sim')
+    gz_sim_launch_path   = PathJoinSubstitution([ros_gz_sim_pkg_path, 'launch', 'gz_sim.launch.py'])
+    gz_spawn_launch_path = PathJoinSubstitution([ros_gz_sim_pkg_path, 'launch', 'gz_spawn_model.launch.py'])
+
+    description_pkg_path  = get_package_share_directory('darwin_description')
+    urdf_file_path = os.path.join(description_pkg_path, 'urdf', 'darwin_lab.urdf')
+    robot_description_content = xacro.process_file(urdf_file_path).toxml()
+       
+    robot_state_publisher_params = [{'robot_description': robot_description_content}]
+
+    # xarm_descrip_pkg_path = get_package_share_directory('xarm_description')
+    # xarm_controller_pkg_path = get_package_share_directory('xarm_controller')
+    # xarm_controller_file = PathJoinSubstitution([xarm_controller_pkg_path, 'config', 'xarm6_controllers.yaml'])
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gz_sim_launch_path),
+            launch_arguments={
+                # 'gz_args':[f'-r ', world_file_path],
+                'on_exit_shutdown':'True',
+            }.items(),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gz_spawn_launch_path),
+            launch_arguments={
+                'world':'default',
+                'topic':'/robot_description',
+                'entity_name': 'darwin',
+                'x': '-4.0',
+                'y': '0.0',
+                'z': '1.0',
+                'Y': '1.5708'
+            }.items(),
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=robot_state_publisher_params
+        ),
+        # Node(
+        #     package='ros_gz_bridge',
+        #     executable='parameter_bridge',
+        #     arguments=[
+        #         '--ros-args', '-p',
+        #         f'config_file:={gz_bridge_params_path}'
+        #     ],
+        #     output='screen'
+        # ),
+        # Node(
+        #     package='rviz2',
+        #     executable='rviz2',
+        #     name='rviz2',
+        #     output='screen',
+        #     arguments=['-d', rviz_config_file],
+        # )
+    ])
