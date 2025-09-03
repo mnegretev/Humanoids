8df86a2b890239939f7b2467f5810a56f5b2d362
Ruth Moreno <ruthmced@gmail.com>
2025-09-02 19:26:25 -0600

    SetupDraft update for ROS2

---
+++ setup/workspace/ROS2_SetupDraft.sh
@@ -0,0 +1,14 @@
+# Gazebo (now ros_gz)
+sudo apt install ros-jazzy-ros-gz -y
+sudo apt install ros-jazzy-ros-gz-sim -y
+
+# ros2_control 
+sudo apt install ros-jazzy-ros2-control -y
+sudo apt install ros-jazzy-ros2-controllers -y
+sudo apt install ros-jazzy-controller-manager -y
+
+# MoveIt2
+sudo apt install ros-jazzy-moveit -y
+
+# Dynamixel
+sudo apt install ros-jazzy-dynamixel-sdk -y
